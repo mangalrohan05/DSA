251dcaf807768c215496ac4400b961157eea9bf5
mangalrohan05 <rohanmangal.2019@gmail.com>
2026-09-15 23:27:05 +0530

    bst

---
+++ TREES/BST/intro.java
@@ -0,0 +1,28 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root == null) return null;
+        while(root.val != val){
+
+            if(val > root.val && root.right != null)
+                root = root.right;
+            else if(val < root.val && root.left != null)
+                root = root.left;
+            else return null;
+        }
+
+        return root;
+    }
+}
---
+++ TREES/BST/kth_smallest.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class kth_smallest {
+    public static void main(String[] args) {
+        ArrayList<Integer> ls = new ArrayList<>();
+
+        ls.get(ls.size()-1);
+    }
+}
