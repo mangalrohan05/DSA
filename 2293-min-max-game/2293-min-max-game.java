8ca86e05c0e516dd77f9471bb784fba7815611b3
Rohan Mangal <rohanmangal.2019@gmail.com>
2026-09-18 03:30:40 +0530

    Time: 0 ms (100%), Space: 45 MB (52.04%) - LeetHub

--- 2293-min-max-game/2293-min-max-game.java
+++ 2293-min-max-game/2293-min-max-game.java
@@ -1,27 +1,20 @@
 class Solution {
     public int minMaxGame(int[] nums) {
-        Queue<Integer> q = new LinkedList<>();
-        for(int val : nums)
-            q.offer(val);
+        if(nums.length == 1) return nums[0];
 
+        int res[] = new int[nums.length / 2];
         boolean min = true;
 
-        while(q.size() > 1){
-            int curr = 0;
+        int x = 0;
 
-            while(curr < q.size() && q.size() >= 2){
-                int a = q.poll();
-                int b = q.poll();
-
-                if(min)
-                    q.offer(Math.min(a, b));
-                else 
-                    q.offer(Math.max(a, b));
-
-                min = !min;
-            }
+        for(int i = 0; i < nums.length-1; i+=2){
+            if(min)
+                res[x++] = Math.min(nums[i], nums[i+1]); 
+            else
+                res[x++] = Math.max(nums[i], nums[i+1]); 
+            min = !min;
         }
 
-        return q.poll();
+        return minMaxGame(res);
     }
 }
